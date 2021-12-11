3ffb3a705f123f08a0e883785577084421c01de2
Matheus de Souza <matheus.zhsouza@gmail.com>
2021-12-11 12:12:42 -0300

    Conceito de blocos, linhas e pontos

---
+++ exercicios/src/fundamentals/FirstProgram.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+import static java.lang.System.out;
+
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        out.println("First Program #01");
+        out.println("First Program #02");
+
+        {
+            ;
+            ;
+            ;
+        }
+    }
+}
