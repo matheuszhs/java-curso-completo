0218a958049b97c82a259fd62700d51ae77f9cd3
Matheus de Souza <matheus.zhsouza@gmail.com>
2021-12-17 06:45:34 -0300

    Calculo e conceito de variáveis e constantes na pratica

---
+++ exercicios/src/fundamentals/CircumferenceArea.java
@@ -0,0 +1,18 @@
+package fundamentals;
+
+import static java.lang.System.out;
+
+public class CircumferenceArea {
+
+    public static void main(String[] args) {
+        double r = 3.4;
+        final double PI = 3.14159;
+
+        double area = PI * r * r;
+        out.println(area);
+
+        r = 10;
+        area = PI * r * r;
+        out.println("Area = " + area + "m2.");
+    }
+}
--- exercicios/src/fundamentals/FirstProgram.java
+++ exercicios/src/fundamentals/FirstProgram.java
@@ -2,11 +2,34 @@ package fundamentals;
 
 import static java.lang.System.out;
 
+/**
+ * This class is...
+ *
+ * @author Matheus
+ * @since JDK1.0
+ * @see
+ */
 public class FirstProgram {
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
+
+        // The code phrase ends with ;
+        // More one comment
+        // End
         out.println("First Program #01");
+
+        /*
+         *   Line 1
+         *   Line 2
+         *   Line 3
+         */
         out.println("First Program #02");
+
+        out.println("End"); // Here work to!
 
         {
             ;
