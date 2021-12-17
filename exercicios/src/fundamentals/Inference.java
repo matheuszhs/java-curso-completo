fd8c2d070ee0b101cd5deeb13acc0f1a8774a0f3
Matheus de Souza <matheus.zhsouza@gmail.com>
2021-12-17 07:44:13 -0300

    Inferência (var)

--- exercicios/src/fundamentals/Inference.java
+++ exercicios/src/fundamentals/Inference.java
@@ -1,5 +1,35 @@
 package fundamentals;
 
+import static java.lang.System.out;
+
 public class Inference {
 
+    public static void main(String[] args) {
+        double a = 4.5; // declare and initialize
+        out.println(a);
+
+        // a = "...";
+
+        var b = 4.5;
+        out.println(b);
+
+        var c = "String";
+        out.println(c);
+
+        c = "Other String";
+        out.println(c);
+
+        // c = 4.5;
+
+        double d; // declare
+        d = 123.45; // initialize
+        out.println(d); // used
+
+        var e = 123.45; // required declare and initialize
+        out.println(e);
+
+        var f = 13; // integer
+        // f = 12.01;  error
+        out.println(f);
+    }
 }
