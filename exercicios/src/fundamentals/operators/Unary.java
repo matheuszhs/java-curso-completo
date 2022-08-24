c389404bf7a415c15e55949a060e3bb02f76f5aa
Matheus Souza <matheus.zhsouza@gmail.com>
2022-08-24 22:53:54 +0100

    Operadores Unários

---
+++ exercicios/src/fundamentals/operators/Unary.java
@@ -0,0 +1,25 @@
+package fundamentals.operators;
+
+public class Unary {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        a++; // a = a + 1
+        a--; // a = a - 1
+
+        ++b; // b = b + 1;
+        --b; // b = b - 1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("Mini challenger");
+        System.out.println(++a == b--);
+        System.out.println(a == b);
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
