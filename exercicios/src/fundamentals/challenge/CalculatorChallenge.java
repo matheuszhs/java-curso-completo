f223ed973366ea61c36696f0111f2443dc4912ca
Matheus Souza <matheus.zhsouza@gmail.com>
2022-08-26 20:16:12 +0100

    Resolução do desafio

--- exercicios/src/fundamentals/challenge/CalculatorChallenge.java
+++ exercicios/src/fundamentals/challenge/CalculatorChallenge.java
@@ -21,25 +21,34 @@ public class CalculatorChallenge {
         System.out.println("Type the operation: ");
         String op = sc.next();
 
-        BigDecimal total = null;
-        switch (op) {
-            case "+":
-                total = n1.add(n2);
-                break;
-            case "-" :
-                total = n1.subtract(n2);
-                break;
-            case "*":
-                total = n1.multiply(n2);
-                break;
-            case "/":
-                total = n1.divide(n2, 2, RoundingMode.HALF_UP);
-                break;
-            case "%":
-                total = n1.remainder(n2);
-                break;
-        }
+        BigDecimal total =
+                op.equals("+") ? n1.add(n2) :
+                        op.equals("-") ? n1.subtract(n2) :
+                                op.equals("*") ? n1.multiply(n2) :
+                                        op.equals("/") ? n1.divide(n2, 2, RoundingMode.HALF_UP) :
+                                                op.equals("%") ? n1.remainder(n2) : new BigDecimal("0");
 
-        System.out.printf("%nTotal: %.2f", total);
+        System.out.printf("%.2f %s %.2f = %.2f %n%n", n1, op, n2, total);
+
+        // Class solution
+        System.out.println("Informe o número: ");
+        double num1 = sc.nextDouble();
+
+        System.out.println("Informe o número: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("Informe a operação: ");
+        String ope = sc.next();
+
+        // Lógica
+        double resultado = "+".equals(ope) ? num1 + num2 : 0;
+        resultado = "-".equals(ope) ? num1 - num2 : resultado;
+        resultado = "*".equals(ope) ? num1 * num2 : resultado;
+        resultado = "/".equals(ope) ? num1 / num2 : resultado;
+        resultado = "%".equals(ope) ? num1 % num2 : resultado;
+
+        System.out.printf("%.2f %s %.2f = %.2f", num1, ope, num2, resultado);
+
+        sc.close();
     }
 }
