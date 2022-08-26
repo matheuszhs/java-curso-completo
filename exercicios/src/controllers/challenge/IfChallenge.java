d9cd021acfc0b38d9c6abd8c958c338f5b51b316
Matheus Souza <matheus.zhsouza@gmail.com>
2022-08-26 22:40:58 +0100

    Desafio If

---
+++ exercicios/src/controllers/challenge/IfChallenge.java
@@ -0,0 +1,13 @@
+package controllers.challenge;
+
+public class IfChallenge {
+
+  public static void main(String[] args) {
+    double grade = 1.3;
+
+    if (grade >= 9.0) /*; wrong place*/ {
+      System.out.println("Honor Board!");
+      System.out.println("You're the best!!");
+    }
+  }
+}
