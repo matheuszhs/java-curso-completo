8d86f4e3a0d35088f88bedf23305de879f725db8
Matheus Souza <matheus.zhsouza@gmail.com>
2022-09-13 00:26:01 +0100

    While determinado

---
+++ exercicios/src/controllers/DeterminedWhile.java
@@ -0,0 +1,14 @@
+package controllers;
+
+public class DeterminedWhile {
+
+  public static void main(String[] args) {
+
+    int count = 1;
+    while (count <= 10) {
+      System.out.printf("Count = %d%n", count);
+      count++;
+    }
+  }
+
+}
