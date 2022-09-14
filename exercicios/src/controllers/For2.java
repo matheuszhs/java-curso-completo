857aed01664c7ad29813ef296eec7cf68760755a
Matheus Souza <matheus.zhsouza@gmail.com>
2022-09-14 02:50:35 +0100

    Decrementando for

---
+++ exercicios/src/controllers/For2.java
@@ -0,0 +1,11 @@
+package controllers;
+
+public class For2 {
+
+  public static void main(String[] args) {
+    for (int i = 10; i >= 0; i-=2) {
+      System.out.printf("Count = %d%n",i);
+    }
+  }
+
+}
