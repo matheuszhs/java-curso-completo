eff24129e7239c7acfc109e3ed378f7b447fcdf7
Matheus Souza <matheus.zhsouza@gmail.com>
2023-04-06 08:07:28 +0100

    For 2 solução da aula

--- exercicios/src/controllers/For2.java
+++ exercicios/src/controllers/For2.java
@@ -6,6 +6,11 @@ public class For2 {
     for (int i = 10; i >= 0; i-=2) {
       System.out.printf("Count = %d%n",i);
     }
+
+    // class solution
+    for (int contador = 10; contador >= 0; contador -=2) {
+      System.out.printf("Contador = %d%n", contador);
+    }
   }
 
 }
