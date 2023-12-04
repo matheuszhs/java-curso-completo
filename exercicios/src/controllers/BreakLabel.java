f2c83faf61c30492b8273cfbbcb1216b0dc1a713
Matheus de Souza <matheus.zhsouza@gmail.com>
2023-12-04 20:23:54 +0000

    BREAK #02

---
+++ exercicios/src/controllers/BreakLabel.java
@@ -0,0 +1,21 @@
+package controllers;
+
+public class BreakLabel {
+
+	public static void main(String[] args) {
+
+		out: for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+
+				if (i == 1) {
+					break out;
+				}
+
+				System.out.printf("[%d %d] ", i, j);
+			}
+			System.out.println();
+		}
+
+		System.out.println("End!");
+	}
+}
