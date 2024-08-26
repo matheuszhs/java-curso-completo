57e2b9318dcc4b07709f64455f418018541c1a8b
Matheus de Souza <matheus.zhsouza@gmail.com>
2024-08-26 09:36:42 +0100

    Mapa mental estrutura de dados

---
+++ mental_mind/Estrutura-dados.mm
@@ -0,0 +1,172 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="Java&#xa;Estrutura de Dados" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1724657810188"><hook NAME="MapStyle" background="#2e3440ff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/dark_nord_template.mm" show_tags="UNDER_NODES"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Ubuntu" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Ubuntu" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<node TEXT="FIFO &amp; LIFO" POSITION="top_or_left" ID="ID_1935786685" CREATED="1724655491108" MODIFIED="1724655584592">
+<node TEXT="First In First Out" ID="ID_1328100535" CREATED="1724655546296" MODIFIED="1724655560093">
+<node TEXT="Fila" ID="ID_1507170535" CREATED="1724657816347" MODIFIED="1724657818982"/>
+</node>
+<node TEXT="Last In First Out" ID="ID_1717692516" CREATED="1724655560890" MODIFIED="1724655568920">
+<node TEXT="Pilha" ID="ID_646838544" CREATED="1724657823035" MODIFIED="1724657826598"/>
+</node>
+</node>
+<node TEXT="Constante vs Variável" POSITION="top_or_left" ID="ID_1701757078" CREATED="1724655573671" MODIFIED="1724655689515">
+<node TEXT="Constantes não mudam" ID="ID_165055144" CREATED="1724655690514" MODIFIED="1724655807061">
+<font BOLD="false" ITALIC="false"/>
+</node>
+<node TEXT="Variáveis podem mudar" ID="ID_1225116063" CREATED="1724655808796" MODIFIED="1724655820483"/>
+</node>
+<node TEXT="Dados vs Estrutura" POSITION="bottom_or_right" ID="ID_1948287238" CREATED="1724655498030" MODIFIED="1724655508249">
+<node TEXT="Dados" POSITION="bottom_or_right" ID="ID_1822199107" CREATED="1724655262124" MODIFIED="1724655456780">
+<node TEXT="Números" ID="ID_722210660" CREATED="1724655463593" MODIFIED="1724655472311"/>
+<node TEXT="Letras" ID="ID_1996401237" CREATED="1724655473643" MODIFIED="1724655476671"/>
+<node TEXT="Arquivos" ID="ID_540429724" CREATED="1724655477218" MODIFIED="1724655479827"/>
+</node>
+<node TEXT="Estrutura" POSITION="bottom_or_right" ID="ID_419433217" CREATED="1724655457639" MODIFIED="1724655461139">
+<node TEXT="Organiza" ID="ID_822673870" CREATED="1724655481937" MODIFIED="1724655486312"/>
+<node TEXT="Administra" ID="ID_1863142550" CREATED="1724655487030" MODIFIED="1724655489436"/>
+</node>
+</node>
+<node TEXT="Estruturas Java" POSITION="bottom_or_right" ID="ID_585667403" CREATED="1724655825077" MODIFIED="1724655862733">
+<node TEXT="; (ponto e vírgula) é obrigatório ao final de uma sentença de código" ID="ID_867035317" CREATED="1724655864842" MODIFIED="1724656092921"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      System.out.println(&quot;Uma senença de código&quot;);
+    </p>
+    <p>
+      System.out.println(&quot;Hello World&quot;);
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="{ } usado para definir um bloco de código" ID="ID_768928490" CREATED="1724655895577" MODIFIED="1724656138140"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      void blocoCodigo( ) {
+    </p>
+    <p>
+      System.out.println(&quot;Uma senença de código&quot;);
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT=". (ponto) pode-se quebrar linha por esta notação" ID="ID_488052860" CREATED="1724655999061" MODIFIED="1724656154062"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      System
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;.out
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;.println(&quot;Uma senença de código&quot;);
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Tree - Árvore" ID="ID_428878367" CREATED="1724656663751" MODIFIED="1724657157802">
+<hook URI="../../../../Pictures/tree-filder.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="Divide-se em Package -&gt; Class -&gt; main" ID="ID_62096002" CREATED="1724657178800" MODIFIED="1724657206106"/>
+</node>
+<node TEXT="Comentários" POSITION="bottom_or_right" ID="ID_833724552" CREATED="1724657269641" MODIFIED="1724657284732">
+<node TEXT="Usar apenas se necessário" ID="ID_1482400359" CREATED="1724657285414" MODIFIED="1724657294022"/>
+<node TEXT="Ser mais conciso com código do que enxer de comentário!!!" ID="ID_1679115018" CREATED="1724657294541" MODIFIED="1724657404104"/>
+</node>
+</node>
+</map>
